ff56d8b61fe8e1a887461d9ee2cbb9e6983e96f2
Ovidiu Predescu <ovidiu@gmail.com>
2014-06-09 23:34:34 -0700

    Added ability to specify width and height.

--- tasks/montage.js
+++ tasks/montage.js
@@ -1,92 +1,98 @@
 module.exports = function (grunt) {
 
-    "use strict";
+  "use strict";
 
-    var exec = require("child_process").exec,
-        path = require("path"),
-        mkdirp = require("mkdirp"),
-        rSpecial = /([!"#$%&'()*+,-.\/:;<=>?@[\]\\^`{}|~])/g;
+  var exec = require("child_process").exec,
+      path = require("path"),
+      mkdirp = require("mkdirp"),
+      rSpecial = /([!"#$%&'()*+,-.\/:;<=>?@[\]\\^`{}|~])/g;
 
-    // Build a CSS rule in the format 'selector { property: value; [... property: value;] }'
-    function buildRule(selector, properties) {
-        return selector + " { " + Object.keys(properties).map(function (property) {
-            return property + ": " + properties[property] + ";";
-        }).join(" ") + " }\n";
+  // Build a CSS rule in the format 'selector { property: value; [... property: value;] }'
+  function buildRule(selector, properties) {
+    return selector + " { " + Object.keys(properties).map(function (property) {
+      return property + ": " + properties[property] + ";";
+    }).join(" ") + " }\n";
+  }
+
+  grunt.registerMultiTask("montage", "Generate CSS sprite sheets and the corresponding stylesheet", function () {
+
+    // It's an async task so make sure Grunt knows this
+    var done = this.async(),
+        cliOptions = "",
+        options = {},
+        defaults = {
+          prefix: ".montage",
+          outputImage: "montage.png",
+          outputStylesheet: "montage.css",
+          baseRules: {},
+          magick: {}
+        };
+
+    // Configuration
+    Object.keys(defaults).forEach(function (option) {
+      if (this.data.options && this.data.options[option] !== undefined) {
+        options[option] = this.data.options[option];
+      } else {
+        options[option] = defaults[option];
+      }
+    }, this);
+
+    if ('size' in this.data.options) {
+      options.width = this.data.options['size'];
+      options.height = this.data.options['height'];
+    } else {
+      options.width = options.height = 16;
     }
 
-    grunt.registerMultiTask("montage", "Generate CSS sprite sheets and the corresponding stylesheet", function () {
+    // Add necessary style rules to the base CSS
+    options.baseRules.background = "url('" + options.outputImage + "') no-repeat";
+    options.baseRules.width = options.width + "px";
+    options.baseRules.height = options.height + "px";
 
-        // It's an async task so make sure Grunt knows this
-        var done = this.async(),
-            cliOptions = "",
-            options = {},
-            defaults = {
-                size: 16,
-                prefix: ".montage",
-                outputImage: "montage.png",
-                outputStylesheet: "montage.css",
-                baseRules: {},
-                magick: {}
-            };
+    // Build ImageMagick montage option string
+    cliOptions = Object.keys(options.magick).map(function (option) {
+      return "-" + option + " " + options.magick[option];
+    }).join(" ");
 
-        // Configuration
-        Object.keys(defaults).forEach(function (option) {
-            if (this.data.options && this.data.options[option] !== undefined) {
-                options[option] = this.data.options[option];
-            } else {
-                options[option] = defaults[option];
-            }
-        }, this);
+    // Iterate over all specified file groups.
+    this.files.forEach(function (files) {
 
-        // Add necessary style rules to the base CSS
-        options.baseRules.background = "url('" + options.outputImage + "') no-repeat";
-        options.baseRules.width = options.size + "px";
-        options.baseRules.height = options.size + "px";
+      // Remove non-existent files from the list
+      var src = files.src.filter(function (file) {
+        if (!grunt.file.exists(file)) {
+          grunt.log.warn("Source file '" + file + "' not found.");
+          return false;
+        }
+        return true;
+      }),
+          dest = path.join(files.dest, options.outputImage),
+          sqrt = Math.sqrt(src.length),
+          rows = Math.floor(sqrt),
+          cols = Math.ceil(sqrt),
+          css = buildRule(options.prefix, options.baseRules);
 
-        // Build ImageMagick montage option string
-        cliOptions = Object.keys(options.magick).map(function (option) {
-            return "-" + option + " " + options.magick[option];
-        }).join(" ");
+      // Create the output directory if necessary (ImageMagick errors if it doesn't exist)
+      if (!grunt.file.exists(files.dest)) {
+        mkdirp(files.dest);
+      }
 
-        // Iterate over all specified file groups.
-        this.files.forEach(function (files) {
-
-            // Remove non-existent files from the list
-            var src = files.src.filter(function (file) {
-                    if (!grunt.file.exists(file)) {
-                        grunt.log.warn("Source file '" + file + "' not found.");
-                        return false;
-                    }
-                    return true;
-                }),
-                dest = path.join(files.dest, options.outputImage),
-                sqrt = Math.sqrt(src.length),
-                rows = Math.floor(sqrt),
-                cols = Math.ceil(sqrt),
-                css = buildRule(options.prefix, options.baseRules);
-
-            // Create the output directory if necessary (ImageMagick errors if it doesn't exist)
-            if (!grunt.file.exists(files.dest)) {
-                mkdirp(files.dest);
-            }
-
-            // Generate a stylesheet
-            css += src.map(function (image, i) {
-                var offsetLeft = (-options.size * (i % cols)) + "px",
-                    offsetTop = (-options.size * Math.floor(i / cols)) + "px",
-                    className = path.basename(image).replace(/\.\w+$/, "").replace(rSpecial, "\\$1");
-                return buildRule(options.prefix + "." + className, {
-                    "background-position": offsetLeft + " " + offsetTop
-                });
-            }).join("");
-
-            grunt.file.write(path.join(files.dest, options.outputStylesheet), css);
-
-            // Execute the ImageMagick montage tool
-            exec("montage -tile " + cols + "x -geometry " + options.size + "x" + options.size + " " + cliOptions + " " + src.join(" ") + " " + dest, function (err) {
-                done();
-            });
+      // Generate a stylesheet
+      css += src.map(function (image, i) {
+        var offsetLeft = (-options.width * (i % cols)) + "px",
+            offsetTop = (-options.height * Math.floor(i / cols)) + "px",
+            className = path.basename(image).replace(/\.\w+$/, "").replace(rSpecial, "\\$1");
+        return buildRule(options.prefix + "." + className, {
+          "background-position": offsetLeft + " " + offsetTop
         });
+      }).join("");
+
+      grunt.file.write(path.join(files.dest, options.outputStylesheet), css);
+
+      // Execute the ImageMagick montage tool
+      exec("montage -tile " + cols + "x -geometry " + options.width + "x" + options.height + " " + cliOptions + " " + src.join(" ") + " " + dest, function (err) {
+        done();
+      });
     });
+  });
 
 };
